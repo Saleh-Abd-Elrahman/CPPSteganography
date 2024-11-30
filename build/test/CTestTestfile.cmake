cdfdb341fbb9086b13f760444da52d63b9ca63e4
Saleh Abd Elrahman <salehabdelrahman14@outlook.com>
2024-11-30 20:36:21 +0100

    Update:Build Remove

--- build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/salehabdelrahman/Documents/GitHub/CPPSteganography/test
-# Build directory: /Users/salehabdelrahman/Documents/GitHub/CPPSteganography/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/salehabdelrahman/Documents/GitHub/CPPSteganography/build/test/test_my_library[1]_include.cmake")
-subdirs("../_deps/googletest-build")
